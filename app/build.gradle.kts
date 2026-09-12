b102f9e9527954692f9abd8d3a5b8bbd95b40beb
tanerari-asaray <tanerari@gmail.com>
2026-09-12 13:12:09 +0300

    Add VIOP app build config

---
+++ app/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "tr.borsatakip.v5"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "tr.borsatakip.v5"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 82
+        versionName = "5.1.31-VIOP"
+    }
+}
